a43dafa433eddb265d87776a829fd8c9b5b3d762
kennylac <kenneth.llerena@outlook.com>
2024-08-12 23:24:33 -0400

    docs: Add comments to explain form code

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+// variables for each form box category
 const form = document.querySelector("form");
 const fullName = document.getElementById("name");
 const email = document.getElementById("email");
@@ -5,6 +6,7 @@ const phone = document.getElementById("phone");
 const subject = document.getElementById("subject");
 const mess = document.getElementById("message");
 
+// What it takes for an email to be sent
 function sendEmail() {
     const bodyMessage = `Full Name: ${fullName.value}<br> Email: ${email.
         value}<br> Phone Number: ${phone.value}<br> Message: ${mess.value}`;
@@ -12,7 +14,7 @@ function sendEmail() {
 
 
     Email.send({
-        SecureToken: "cbe9bea9-333c-4f64-b2ca-4e05e2553ee6 ",
+        SecureToken: "cbe9bea9-333c-4f64-b2ca-4e05e2553ee6 ", //Individuals can't see credentials 
         // Host : "smtp.elasticemail.com",
         // Username : "jeanlkenneth@gmail.com",
         // Password : "3A21FEA22C32366EC4B6A12CF192D395B099",
@@ -20,8 +22,12 @@ function sendEmail() {
         From : "jeanlkenneth@gmail.com",
         Subject : subject.value,
         Body : bodyMessage
+
+        // What happens after an email is sent
     }).then(
       message => {
+
+        //Display Success pop up
         if (message == "OK") {
             Swal.fire({
                 title: "Success!",
@@ -30,28 +36,28 @@ function sendEmail() {
               });
         }
       }
-
-
-
     );
 
 }
 
-
+//Check to see if the form boxes are empty
 function checkInputs() {
     const items = document.querySelectorAll(".item");
 
+    
     for (const item of items) {
+        //If a form box was left blank, add the error class
         if (item.value == "") {
             item.classList.add("error");
             item.parentElement.classList.add("error");
 
         }
-
+        //If the email box (2 item in list) is blank call back checkEmail function
         if (items[1].value != "") {
             checkEmail();
         }
 
+        //Need to research purpose
         items[1].addEventListener("keyup", () => {
             checkEmail();
         });
@@ -71,6 +77,7 @@ function checkInputs() {
     }
 }
 
+// A regular expression to ensure user puts a valid email address
 function checkEmail() {
     const emailRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
 
@@ -97,6 +104,7 @@ function checkEmail() {
     
 }
 
+//What happens when user hits the submit button
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     checkInputs();
@@ -105,7 +113,7 @@ form.addEventListener("submit", (e) => {
     contains("error") && !mess.classList.contains("error")) {
         sendEmail();
 
-        form.reset();
+        form.reset(); //reset the form to blank
         return false;
     }
 
